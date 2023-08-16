fd1424e93a45a7ff1ff8568c29100b18335fc3ba
Aryan <rajaryanhome@gmail.com>
2023-08-16 12:40:07 +0530

    Completed the logic functions

--- Main.java
+++ Main.java
@@ -8,27 +8,95 @@ class ChandrayaanSpacecraft {
     }
 
     public void moveForward() {
-        
+        switch (direction) {
+            case 'N':
+                position[1]++;
+                break;
+            case 'S':
+                position[1]--;
+                break;
+            case 'E':
+                position[0]++;
+                break;
+            case 'W':
+                position[0]--;
+                break;
+            case 'U':
+                position[2]++;
+                break;
+            case 'D':
+                position[2]--;
+                break;
+        }
     }
 
     public void moveBackward() {
-        
+        switch (direction) {
+            case 'N':
+                position[1]--;
+                break;
+            case 'S':
+                position[1]++;
+                break;
+            case 'E':
+                position[0]--;
+                break;
+            case 'W':
+                position[0]++;
+                break;
+            case 'U':
+                position[2]--;
+                break;
+            case 'D':
+                position[2]++;
+                break;
+        }
     }
 
     public void turnLeft() {
-    
+        switch (direction) {
+            case 'N':
+                direction = 'W';
+                break;
+            case 'S':
+                direction = 'E';
+                break;
+            case 'E':
+                direction = 'N';
+                break;
+            case 'W':
+                direction = 'S';
+                break;
+        }
     }
 
     public void turnRight() {
-        
+        switch (direction) {
+            case 'N':
+                direction = 'E';
+                break;
+            case 'S':
+                direction = 'W';
+                break;
+            case 'E':
+                direction = 'S';
+                break;
+            case 'W':
+                direction = 'N';
+                break;
+        }
     }
 
     public void turnUp() {
-        
+        if (direction != 'U') {
+            direction = 'U';
+        }
     }
 
     public void turnDown() {
-       
+        if (direction != 'D') {
+            direction = 'D';
+        }
     }
 
     public int[] executeCommands(char[] commands) {
